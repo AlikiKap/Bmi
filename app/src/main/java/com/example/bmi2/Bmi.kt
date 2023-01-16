d3a52869e83143f0141e82854af1d6266061033b
Aliki Kapasakalidi <aliki02.kapasakalidi@gmail.com>
2023-01-16 13:44:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bmi2/Bmi.kt
@@ -0,0 +1,54 @@
+package com.example.bmi2
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Bmi() {
+    var heightInput: String by remember { mutableStateOf("") }
+    var weightInput: String by remember { mutableStateOf("") }
+    val height = heightInput.toFloatOrNull() ?: 0.0f
+    val weight = weightInput.toIntOrNull() ?: 0
+    val bmi = if (weight > 0 && height > 0) weight / (height * height) else 0.0
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.Body_mass_index),
+            fontSize = 24.sp,
+            color = MaterialTheme.colors.primary,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp)
+        )
+        OutlinedTextField(value = heightInput,
+            onValueChange ={heightInput = it.replace(',','.')},
+            label = { Text(stringResource(R.string.Height)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(value = weightInput,
+            onValueChange ={weightInput = it.replace(',','.')},
+            label = { Text(stringResource(R.string.Weight)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(text = stringResource(R.string.answer,String.format("%.2f",bmi).replace(",", ".")))
+    }
+
+}
